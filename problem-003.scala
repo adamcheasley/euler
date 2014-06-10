2b9a61f4604ced8aa0c668f2637cc7313df8a095
Adam Forsythe-Cheasley <adam@netsight.co.uk>
2014-06-10 14:02:50 +0100

    first attempt at generating largest prime factor. This issue here is that when the number gets sufficiently large, the isPrime method creates a list that is too long for scala to handle.

--- problem-003.scala
+++ problem-003.scala
@@ -2,33 +2,27 @@
 
 object ProblemTwo {
 
-  def isPrime(i: Int): Boolean = {
-    if (i <= 1) false
-    else if (i == 2) true
-    else !(2 to (i-1)).exists(x => i % x == 0)
+  def isPrime(i: Long): Boolean = {
+    if (i <= 1L) false
+    else if (i == 2L) true
+    else !(2L to ((i/2)-1L)).exists(x => i % x == 0L)
   }
 
-  def findFactors(i: Int): List[Int] = {
-    def rec(x: Int, toTest: List[Int], factors: List[Int]): List[Int] = {
-      if (toTest.isEmpty) factors
-      else if (i % toTest.head == 0) rec(x, toTest.tail, factors :+ toTest.head)
-      else rec(x, toTest.tail, factors)
+  def findGreatesPrimeFactor(i: Long): Long = {
+    def rec(x: Long, toTest: Long, factor: Long): Long = {
+      if (toTest == (i/2)) factor
+      else if (i % toTest == 0 && isPrime(toTest))
+        rec(x, toTest + 1L, toTest)
+      else rec(x, toTest + 1L, factor)
      }
-    rec(i, List.range(2, (i-1)), List())
+    rec(i, 2L, 2L)
   }
-
-  // def findPrimeFactors(x: Int): List[Int] = {
-  //   def rec(n: Int, toTest: List[Int], primeFactors: List[Int]): List[Int] = {
-  //     //if (n == (x/2)) primeFactors
-  //     //else
-  //   }
-  // }
 }
 
 
 object Main {
   def main(args: Array[String]) {
     import ProblemTwo._
-    println(findFactors(20))
+    println(findGreatesPrimeFactor(600851475143L))
   }
 }
